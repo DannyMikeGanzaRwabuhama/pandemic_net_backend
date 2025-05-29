cd5dbcf75ee7ccbb1ba5d30e37187c68fffaa648
Byukusenge-Andrew <andrewbyukusenge4@gmail.com>
2025-05-29 08:59:37 +0200

    added some entry implementaion

--- src/main/java/com/app/pandemicnet/repository/DataCentreRepository.java
+++ src/main/java/com/app/pandemicnet/repository/DataCentreRepository.java
@@ -1,13 +1,12 @@
 package com.app.pandemicnet.repository;
 
-import com.app.pandemicnet.model.User;
+import com.app.pandemicnet.model.DataCentre;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findUserByUniqueId(UUID userUniqueId);
+public interface DataCentreRepository extends JpaRepository<DataCentre, Long> {
+    DataCentre findDataCentreByUniqueId(UUID dataCentreUniqueId);
 }
