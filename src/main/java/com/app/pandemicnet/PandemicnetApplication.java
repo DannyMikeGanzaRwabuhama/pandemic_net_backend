2c277dba4d4bb49404624a8b2f566d4acd913ea4
christian783 <christiantc0000@gmail.com>
2025-05-29 10:34:01 +0200

    Added data centre component

--- src/main/java/com/app/pandemicnet/PandemicnetApplication.java
+++ src/main/java/com/app/pandemicnet/PandemicnetApplication.java
@@ -1,18 +1,9 @@
 package com.app.pandemicnet;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(
-		info = @Info(
-				title = "PandemicNet",
-				description = "backend documentation for pandemicNet",
-				version = "v1.0.0"
-		)
-)
 public class PandemicnetApplication {
 
 	public static void main(String[] args) {
