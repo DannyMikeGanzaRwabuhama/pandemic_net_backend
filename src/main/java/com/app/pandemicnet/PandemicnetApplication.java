e3f5a4d177db8202e5bde6eec1b41f72a2a71a4a
mike <unknownentity5648@gmail.com>
2025-05-29 07:26:34 +0200

    Fixed errors

---
+++ src/main/java/com/app/pandemicnet/PandemicnetApplication.java
@@ -0,0 +1,13 @@
+package com.app.pandemicnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandemicnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandemicnetApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/app/pandemicnet/model/DataCentre.java
@@ -0,0 +1,29 @@
+package com.app.pandemicnet.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.UUID;
+
+@Entity
+@Table(name = "data_centre")
+@Data
+public class DataCentre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "unique_id", nullable = false, unique = true)
+    private UUID uniqueId = UUID.randomUUID();
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Double latitude;
+
+    @Column(nullable = false)
+    private Double longitude;
+
+    @Column(nullable = false)
+    private String address;
+}
